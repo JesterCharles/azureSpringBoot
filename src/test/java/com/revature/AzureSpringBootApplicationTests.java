e6b27871a83e3d665653268f8f36da74697e704d
JesterCharles <charles.jester@revature.com>
2022-02-24 00:10:03 -0500

    Updated a false test to see what happens to the pipeline

--- src/test/java/com/revature/AzureSpringBootApplicationTests.java
+++ src/test/java/com/revature/AzureSpringBootApplicationTests.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +11,11 @@ class AzureSpringBootApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void testTrue() {
+		assertThat(false);
+	}
+	
 
 }
